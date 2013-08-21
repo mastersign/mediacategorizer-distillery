762d277780b6d5bb57acc9634e31f04bc88918cf
mastersign <dev@mastersign.de>
2013-08-21 16:11:52 +0200

    added JavaScript code for the word cloud UI

--- resources/distillery.js
+++ resources/distillery.js
@@ -27,3 +27,55 @@ function word(word_id) {
     innerpage("word");
   });
 }
+
+var clouds = {};
+
+function register_cloud_data(cloud_id, cloud_data) {
+  clouds[cloud_id] = cloud_data;
+}
+
+function rect_contains(r, x, y) {
+  return x >= r.x && x <= (r.x + r.w) && y >= r.y && y <= (r.y + r.h);
+}
+
+function get_word_id(event) {
+  var id = event.data.cloud_id;
+  var x = event.clientX - event.currentTarget.offsetLeft;
+  var y = event.clientY - event.currentTarget.offsetTop;
+  var words = clouds[id];
+  for (var i = 0; i < words.length; i++) {
+    var w = words[i];
+    if (rect_contains(w.r, x, y)) {
+      return w.id;
+    }
+  }
+  return null;
+}
+
+function cloud_click_handler(event) {
+  var word_id = get_word_id(event);
+  if (word_id) {
+    word(word_id);
+  }
+}
+
+function cloud_mousemove_handler(event) {
+  var img = event.currentTarget;
+  var word_id = get_word_id(event);
+  if (word_id) {
+    img.style.cursor = "pointer";
+  } else {
+    img.style.cursor = "default";
+  }
+}
+
+function register_cloud_handler(i, cloud_figure) {
+  var id = cloud_figure.getAttribute("data-cloud-id");
+  var img = $("img.wordcloud", cloud_figure)[0];
+  $(img).on("click", { cloud_id: id }, cloud_click_handler);
+  $(img).on("mousemove", { cloud_id: id }, cloud_mousemove_handler);
+}
+
+$(function() {
+  $.each($("figure.wordcloud"), register_cloud_handler);
+});
