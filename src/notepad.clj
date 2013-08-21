60b40c47b401c36c3b4807654bb8019cc3dec132
mastersign <dev@mastersign.de>
2013-08-21 17:08:34 +0200

    fixed typo in notepad

--- src/notepad.clj
+++ src/notepad.clj
@@ -1,7 +1,6 @@
 (ns notepad
   (:require [clojure.pprint :refer [pprint]])
   ;(:require [distillery.data :refer :all])
-  ;(:require [distillery.tasts :refer :all])
+  ;(:require [distillery.tasks :refer :all])
   ;(:require [mastersign.wordcloud :refer :all])
   )
-
