424c65ce12d3c79632af8ccf8917e1665e14dd5f
mastersign <dev@mastersign.de>
2013-08-29 14:49:03 +0200

    added comments to config.clj

--- src/distillery/config.clj
+++ src/distillery/config.clj
@@ -1,15 +1,18 @@
 (ns distillery.config
   (:require [clojure.java.io :refer (resource)]))
 
-
-(def best-phrases-only true)
+;; ## Preprocessing
 
 (def blacklist-resource (resource "top10000de.txt"))
 (def blacklist-max-size 3000)
+
+(def best-phrases-only true)
 
 (def min-confidence 0.400)
 (def good-confidence 0.700)
 
 (def min-relative-appearance (/ 1.0 4.0))
+
+;; ## Output Generation
 
 (def skip-wordclouds false)
