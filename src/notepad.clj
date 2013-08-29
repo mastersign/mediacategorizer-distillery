16877b255ac3ed38bbef0961cbe7edd8d3c139ef
mastersign <dev@mastersign.de>
2013-08-29 14:51:23 +0200

    extended notepad.clj

--- src/notepad.clj
+++ src/notepad.clj
@@ -1,14 +1,15 @@
 (ns notepad
   (:require [clojure.test :refer :all])
   (:require [clojure.pprint :refer [pprint]])
-  (:require [mastersign.wordcloud-test])
-  (:require [distillery.core-test])
+  ;(:require [mastersign.wordcloud-test])
   ;(:require [distillery.data :refer :all])
   ;(:require [distillery.tasks :refer :all])
-  ;(:require [mastersign.wordcloud :refer :all])
+  (:require [distillery.core-test])
   )
 
 ;(run-tests 'mastersign.wordcloud-test)
+
+;(distillery.core-test/test-playground)
 
 ;(distillery.core-test/test-resources)
 
@@ -17,5 +18,4 @@
 ;(distillery.core-test/test-index)
 
 ;(distillery.core-test/test-complete)
-
 
