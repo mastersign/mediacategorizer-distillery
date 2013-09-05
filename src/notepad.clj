d115c8bece9ff17053cc3c106cd8774fd9663658
Tobias Kiertscher <dev@mastesign.de>
2013-09-05 16:20:36 +0200

    added mechanism for offlining category resources during development

--- src/notepad.clj
+++ src/notepad.clj
@@ -23,3 +23,46 @@
 ;(distillery.core-test/test-complete)
 
 ;(distillery.core-test/test-show-main-page)
+
+; -----------------
+
+;(let [dldir "C:\\Temp\\wiki\\"] (download-resources dldir) (adjust-job-for-offline dldir))
+;(adjust-job-for-offline "C:\\Temp\wiki\\")
+
+(defn download-resources
+  "Downloads all resources into dldir."
+  [dldir]
+  (let [ress (mapcat (fn [c] (map #(vector (:type %) (:url %))
+                                  (:resources c)))
+                     (:categories distillery.core-test/job-descr))]
+    (doseq [[typ url] ress]
+      (let [url* (java.net.URL. url)
+            p (.getPath url*)]
+        (when-not (= (.getProtocol url*) "file")
+          (let [n (.substring p (inc (.lastIndexOf p "/")))
+                f (str dldir n ".offline")]
+            (spit f (slurp (if (= :wikipedia typ)
+                             (str url "?action=render")
+                             url)))))))))
+
+(defn adjust-job-for-offline
+  [dldir]
+  (intern 'distillery.core-test 'job-descr
+    (update-in
+     distillery.core-test/job-descr [:categories]
+     (fn [cats]
+       (vec (map
+             (fn [cat]
+               (update-in
+                cat [:resources]
+                (fn [ress]
+                  (vec (map
+                        (fn [r]
+                          (let [url (:url r)
+                                p (.getPath (java.net.URL. url))
+                                n (.substring p (inc (.lastIndexOf p "/")))
+                                f (str dldir n ".offline")]
+                            (assoc r :url (str "file:///" f))))
+                        ress)))))
+             cats))))))
+
