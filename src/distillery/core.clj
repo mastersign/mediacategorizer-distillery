a510893a7d47d68b8077fdd3db2ed7abe4ccfa6f
mastersign <dev@mastersign.de>
2013-11-18 16:23:39 +0100

    use progress trace macros

--- src/distillery/core.clj
+++ src/distillery/core.clj
@@ -7,29 +7,32 @@
 
 (defn run-complete
   [job-descr]
-  (let [job (-> job-descr
-                dt/load-speech-recognition-results
-                dt/analyze-speech-recognition-results
-                dt/load-categories
-                dt/analyze-categories
-                dt/match-videos
-                dt/lookup-categories-matches
-                dt/matching-stats)]
+  (let [job (dt/process-pipeline
+             "Preparation"
+             job-descr
+             dt/load-speech-recognition-results
+             dt/analyze-speech-recognition-results
+             dt/load-categories
+             dt/analyze-categories
+             dt/match-videos
+             dt/lookup-categories-matches
+             dt/matching-stats)]
     (trace-block
      "Output generation"
      (dt/prepare-output-dir job)
-     (doall ((dt/map-fn) #(% job)
-             [dt/create-index-page
-              dt/create-categories-page
-              dt/create-category-pages
-              dt/create-videos-page
-              dt/create-video-pages]))))
-  (System/exit 0)
+     (dt/process-task-group
+      "Output" #(% job)
+      [dt/create-index-page
+       dt/create-categories-page
+       dt/create-category-pages
+       dt/create-videos-page
+       dt/create-video-pages])))
   nil)
 
 (defn -main
   [job-file]
   (-> job-file
       load-data
-      run-complete))
-
+      run-complete)
+  (System/exit 0))
+
