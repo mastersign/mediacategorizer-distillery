8f431dcae693612e1d8c015a98feca2b37416b54
mastersign <dev@mastersign.de>
2013-11-21 16:10:24 +0100

    made first page of glossary visible (untested)

--- src/distillery/view/glossary.clj
+++ src/distillery/view/glossary.clj
@@ -11,10 +11,14 @@
   [letter]
   (if (= \? letter) "SYM" (str letter)))
 
+(defn- letter-list
+  []
+  (map char (concat (range 65 91) [63])))
+
 (defn- render-glossary-navigation
   "Creates the HTML for the navigation bar of a glossary."
   [pindex]
-  (let [letters (map char (concat (range 65 91) [63]))]
+  (let [letters (letter-list)]
     (div "glossary-nav"
       (map (fn [l]
              (if (contains? pindex l)
@@ -22,17 +26,28 @@
                (str l "  ")))
            letters))))
 
+(defn- first-letter
+  [pindex]
+  (let [index-letters (map first pindex)]
+    (->> (letter-list)
+         (map (fn [l] (map #(== % l) index-letters)))
+         first)))
+
 (defn- render-glossary-partition
   "Creates the HTML for a partion of a glossary."
-  [[letter index-part]]
-  {:tag :div
-   :attrs {:id (str "glossary-part-" (glossary-partition-id letter)) :class "glossary-part"}
-   :content [(ulist "glossary" (map render-glossary-word (vals index-part)))]})
+  [first-letter [letter index-part]]
+  (let [is-first-letter (== letter first-letter)]
+    {:tag :div
+     :attrs {:id (str "glossary-part-" (glossary-partition-id letter))
+             :class "glossary-part"
+             :display (if is-first-letter "inherit" "none")}
+     :content [(ulist "glossary" (map render-glossary-word (vals index-part)))]}))
 
 (defn render-glossary
   "Create the HTML for a glossary."
   [pindex]
-  (vec (cons
-        (render-glossary-navigation pindex)
-        (map render-glossary-partition pindex))))
-
+  (let [fl (first-letter pindex)]
+    (vec (cons
+          (render-glossary-navigation pindex)
+          (map (partial render-glossary-partition fl) pindex)))))
+
