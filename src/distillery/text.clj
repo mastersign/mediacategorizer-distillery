48fc831fe9ff0ec6d39fedac7ca4ecf91943d967
mastersign <dev@mastersign.de>
2013-12-06 15:42:27 +0100

    added function for extern string resources

---
+++ src/distillery/text.clj
@@ -0,0 +1,11 @@
+(ns distillery.text
+  (:require [clojure.java.io :refer (resource)])
+  (:require [distillery.data :refer (load-data)]))
+
+(def resources (load-data (resource "text.edn")))
+
+(defn txt
+  [id]
+  (let [v (get resources id)]
+    (if (keyword? v) (txt v) v)))
+
