ce98e12c2665780dac3bdf9187b604c56561c131
mastersign <dev@mastersign.de>
2013-12-17 12:11:24 +0100

    set version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject distillery "0.2.0-SNAPSHOT"
+(defproject distillery "0.2.0"
   :description "A Clojure application to filter the most relevant words from speech recognition results."
   :url "http://informatik.fh-brandenburg.de/~kiertsch/"
   :license {:name "None"
